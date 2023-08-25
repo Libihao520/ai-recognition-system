dc09a912ed8296797f1f7956415e783f746ae97b
Libihao <1074775789@qq.com>
2023-08-25 22:56:24 +0800

    feat：pinia构建用户仓库与token持久化

--- Vue3-ai-system-admin/src/App.vue
+++ Vue3-ai-system-admin/src/App.vue
@@ -1,8 +1,16 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from '@/stores/user'
 
+const userStore = useUserStore()
+</script>
 <template>
   <test-demo></test-demo>
-  <div><el-button>按钮</el-button></div>
+  <div>
+    <el-button>按钮</el-button>
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('bearer')">登录</el-button>
+    <el-button @click="userStore.removeToken()">退出</el-button>
+  </div>
 </template>
 
 <style scoped></style>
