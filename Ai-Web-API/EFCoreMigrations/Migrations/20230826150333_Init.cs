b840a4926140b2f00ef8ce4de2508e5b37a64ab5
Libihao <1074775789@qq.com>
2023-08-26 23:04:11 +0800

    feat：初始化efcore的migrations

---
+++ Ai-Web-API/EFCoreMigrations/Migrations/20230826150333_Init.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreMigrations.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateUserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "yolotbs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cls = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    sbjgCount = table.Column<int>(type: "int", nullable: false),
+                    IsManualReview = table.Column<bool>(type: "bit", nullable: false),
+                    sbzqCount = table.Column<int>(type: "int", nullable: false),
+                    rgmsCount = table.Column<int>(type: "int", nullable: false),
+                    zql = table.Column<double>(type: "float", nullable: false),
+                    zhl = table.Column<double>(type: "float", nullable: false),
+                    CreateUserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yolotbs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "yolotbs");
+        }
+    }
+}
