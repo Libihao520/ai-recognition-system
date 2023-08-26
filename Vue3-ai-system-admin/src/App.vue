3aa6c4f161a19efff7ac8c51f23f7bdc83d94876
Libihao <1074775789@qq.com>
2023-08-26 11:57:16 +0800

    feat：登录页面

--- Vue3-ai-system-admin/src/App.vue
+++ Vue3-ai-system-admin/src/App.vue
@@ -1,18 +1,6 @@
-<script setup>
-import { useUserStore } from '@/stores'
-
-const userStore = useUserStore()
-</script>
+<script setup></script>
 <template>
   <router-view></router-view>
-  <hr />
-  <test-demo></test-demo>
-  <div>
-    <el-button>按钮</el-button>
-    <p>{{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('bearer')">登录</el-button>
-    <el-button @click="userStore.removeToken()">退出</el-button>
-  </div>
 </template>
 
 <style scoped></style>
