f007f6d78b6b2c95a00759384027f65fe86dec90
Libihao <1074775789@qq.com>
2023-08-27 17:01:49 +0800

    feat：测试组件

--- Vue3-ai-system-admin/src/views/yolo/yoloCar.vue
+++ Vue3-ai-system-admin/src/views/yolo/yoloCar.vue
@@ -1,1 +1,7 @@
-<template>识别车辆数据</template>
+<template>
+  <page-containel title="车辆识别详情"
+    ><template #extra><el-button>测试按钮</el-button></template>
+    主体：表格
+  </page-containel>
+</template>
+<style lang="scss" scoped></style>
--- Vue3-ai-system-admin/src/views/yolo/yoloPkq.vue
+++ Vue3-ai-system-admin/src/views/yolo/yoloPkq.vue
@@ -1,1 +1,7 @@
-<template>识别皮卡丘数据</template>
+<template>
+  <page-containel title="皮卡丘识别详情"
+    ><template #extra><el-button>测试按钮</el-button></template>
+    主体：表格
+  </page-containel>
+</template>
+<style lang="scss" scoped></style>
