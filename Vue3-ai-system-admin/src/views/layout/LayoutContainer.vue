fd54dd6a53e0eec08e07fd784d6d6cda5dae4abc
Libihao <1074775789@qq.com>
2023-08-27 12:58:43 +0800

    feat：layout架子

--- Vue3-ai-system-admin/src/views/layout/LayoutContainer.vue
+++ Vue3-ai-system-admin/src/views/layout/LayoutContainer.vue
@@ -1,6 +1,147 @@
+<script setup>
+import {
+  Odometer,
+  FolderOpened,
+  Cpu,
+  List,
+  UserFilled,
+  User,
+  Crop,
+  EditPen,
+  SwitchButton,
+  CaretBottom
+} from '@element-plus/icons-vue'
+import avatar from '@/assets/default.png'
+</script>
+
 <template>
-  <div>
-    布局架子
-    <router-view></router-view>
-  </div>
+  <el-container class="layout-container">
+    <el-aside width="200px">
+      <div class="el-aside__logo"></div>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#232323"
+        :default-active="$route.path"
+        text-color="#fff"
+        router
+      >
+        <el-menu-item index="/sjdp/yoloSjdp">
+          <el-icon><Odometer /></el-icon>
+          <span>数据大屏</span>
+        </el-menu-item>
+
+        <el-menu-item index="/yolo/yolorecognition">
+          <el-icon><Cpu /></el-icon>
+          <span>模型识别入口</span>
+        </el-menu-item>
+        <el-sub-menu index="/yolo">
+          <template #title>
+            <el-icon><FolderOpened /></el-icon>
+            <span>识别结果表单</span>
+          </template>
+          <el-menu-item index="/yolo/yolopkq">
+            <el-icon><List /></el-icon>
+            <span>皮卡丘表单</span>
+          </el-menu-item>
+
+          <el-menu-item index="/yolo/yolocar">
+            <el-icon><List /></el-icon>
+            <span>车辆表单</span>
+          </el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="/user">
+          <template #title>
+            <el-icon><UserFilled /></el-icon>
+            <span>个人中心</span>
+          </template>
+
+          <el-menu-item index="/user/profile">
+            <el-icon><User /></el-icon>
+            <span>基本资料</span>
+          </el-menu-item>
+          <el-menu-item index="/user/avatar">
+            <el-icon><Crop /></el-icon>
+            <span>更换头像</span>
+          </el-menu-item>
+          <el-menu-item index="/user/password">
+            <el-icon><EditPen /></el-icon>
+            <span>重置密码</span>
+          </el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <div>当前用户：<strong>超级管理员kb哥</strong></div>
+        <el-dropdown placement="bottom-end">
+          <span class="el-dropdown__box">
+            <el-avatar :src="avatar" />
+            <el-icon><CaretBottom /></el-icon>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item command="profile" :icon="User"
+                >基本资料</el-dropdown-item
+              >
+              <el-dropdown-item command="avatar" :icon="Crop"
+                >更换头像</el-dropdown-item
+              >
+              <el-dropdown-item command="password" :icon="EditPen"
+                >重置密码</el-dropdown-item
+              >
+              <el-dropdown-item command="logout" :icon="SwitchButton"
+                >退出登录</el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>大事件 ©2023 Created by 黑马程序员</el-footer>
+    </el-container>
+  </el-container>
 </template>
+
+<style lang="scss" scoped>
+.layout-container {
+  height: 100vh;
+  .el-aside {
+    background-color: #232323;
+    &__logo {
+      height: 120px;
+      background: url('@/assets/logodes.png') no-repeat center / 120px auto;
+    }
+    .el-menu {
+      border-right: none;
+    }
+  }
+  .el-header {
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .el-dropdown__box {
+      display: flex;
+      align-items: center;
+      .el-icon {
+        color: #999;
+        margin-left: 10px;
+      }
+
+      &:active,
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+  .el-footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    color: #666;
+  }
+}
+</style>
