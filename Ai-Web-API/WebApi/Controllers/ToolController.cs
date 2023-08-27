469d5546fc7690c49ddeabac65e6f3c7e72cfe1a
Libihao <1074775789@qq.com>
2023-08-27 17:19:30 +0800

    feat：tool添加yolo表单初始化数据

--- Ai-Web-API/WebApi/Controllers/ToolController.cs
+++ Ai-Web-API/WebApi/Controllers/ToolController.cs
@@ -27,8 +27,19 @@ public class ToolController : ControllerBase
             CreateUserId = 0,
             IsDeleted = 0
         };
+        var yolotbs = new Yolotbs()
+        {
+            Cls = "皮卡丘",
+            sbjgCount = 10,
+            IsManualReview = false,
+            sbzqCount = 10,
+            rgmsCount = 10,
+            zql = 99.99,
+            zhl = 99.99
+        };
 
         _context.Users.Add(user);
+        _context.yolotbs.Add(yolotbs);
         _context.SaveChanges();
         return "ok";
     }
