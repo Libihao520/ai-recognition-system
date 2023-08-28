0325955991e314ae4c2cc6539e5f446cbd344cde
Libihao <1074775789@qq.com>
2023-08-28 23:07:37 +0800

    feat：封装抽屉，编辑与添加时调用抽屉方法，编辑穿参，添加传空

---
+++ Vue3-ai-system-admin/src/views/yolo/yoloEdit.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+//控制抽屉显示隐藏
+const visibleDrawer = ref(false)
+
+const open = (row) => {
+  visibleDrawer.value = true
+  console.log(row)
+}
+
+defineExpose({
+  open
+})
+</script>
+<template>
+  <!-- 抽屉 -->
+  <el-drawer v-model="visibleDrawer" title="大标题" size="50%">
+    <span>Hi there!</span></el-drawer
+  >
+</template>
