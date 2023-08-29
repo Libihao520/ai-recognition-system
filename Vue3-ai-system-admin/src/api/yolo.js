c2caa98ceceb6d553d9b54bf206e0254fdbe697a
Libihao <1074775789@qq.com>
2023-08-29 23:59:46 +0800

    feat：添加数据api

--- Vue3-ai-system-admin/src/api/yolo.js
+++ Vue3-ai-system-admin/src/api/yolo.js
@@ -3,6 +3,12 @@ import request from '@/utils/request'
 //获取pkq表单数据
 export const GetPkqTbService = () => request.get('/yolo/yolopkq')
 
+//上传照片识别
 export const PutPhotoService = (photo, name) => {
   return request.put('/yolo/PutPhoto', { photo, name })
 }
+
+//皮卡丘表单手动添加
+//data需要一个formData格式的对象
+export const PubListPkqTbService = (data) =>
+  request.post('/yolo/PutDataTb', data)
