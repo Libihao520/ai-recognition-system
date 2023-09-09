297e7d64f141ac81fe93fbc6e718848f7d3f45e1
Libihao <1074775789@qq.com>
2023-09-10 00:25:29 +0800

    feat：前端解决跨域问题

--- Vue3-ai-system-admin/src/utils/request.js
+++ Vue3-ai-system-admin/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useUserStore } from '@/stores'
 import { ElMessage } from 'element-plus'
 import router from '../router'
-const baseURL = 'http://localhost:5157/api'
+const baseURL = '/api'
 
 const instance = axios.create({
   //基础地址，超时时间
