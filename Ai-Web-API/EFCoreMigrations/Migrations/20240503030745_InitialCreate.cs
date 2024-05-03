fe6e7e3c8898466e28b2b95eb7880ce8ec9d37fd
Libihao <1074775789@qq.com>
2024-05-03 15:08:06 +0800

    feat：数据迁移

---
+++ Ai-Web-API/EFCoreMigrations/Migrations/20240503030745_InitialCreate.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    PhotosId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Photobase64 = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.PhotosId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreateUserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    IsDeleted = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "yolotbs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cls = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    sbjgCount = table.Column<int>(type: "int", nullable: false),
+                    IsManualReview = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    sbzqCount = table.Column<int>(type: "int", nullable: false),
+                    rgmsCount = table.Column<int>(type: "int", nullable: false),
+                    zql = table.Column<double>(type: "double", nullable: false),
+                    zhl = table.Column<double>(type: "double", nullable: false),
+                    PhotosId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateUserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    IsDeleted = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yolotbs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_yolotbs_Photos_PhotosId",
+                        column: x => x.PhotosId,
+                        principalTable: "Photos",
+                        principalColumn: "PhotosId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_yolotbs_PhotosId",
+                table: "yolotbs",
+                column: "PhotosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "yolotbs");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
--- Ai-Web-API/EFCoreMigrations/MyDbContext.cs
+++ Ai-Web-API/EFCoreMigrations/MyDbContext.cs
@@ -8,11 +8,22 @@ public class MyDbContext : DbContext
     public DbSet<Users> Users { get; set; }
 
     public DbSet<Yolotbs> yolotbs { get; set; }
-    
+
     public DbSet<Photos> Photos { get; set; }
 
-    //注入方式配置
+    // 注入方式配置
     public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
     {
+    }
+
+    //数据库迁移
+    //dotnet ef migrations add InitialCreate
+    //dotnet ef database update
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        //数据库连接字符串
+        optionsBuilder.UseMySql("server=127.0.0.1;port=3306;database=lbhdatabase;user=root;password=1qazZAQ!;",
+            new MySqlServerVersion(new Version(8, 0, 33)));
     }
 }
