1d8e47367e53035e881032506adad2a42248d054
Libihao <1074775789@qq.com>
2024-06-23 11:59:46 +0800

    feat：添加注释

--- Ai-Web-API/Model/Dto/TestPapers/mathematicsRes.cs
+++ Ai-Web-API/Model/Dto/TestPapers/mathematicsRes.cs
@@ -2,27 +2,37 @@ namespace Model.Dto.TestPapers;
 
 public class MathematicsRes
 {
-
+    /// <summary>
+    /// 单选题
+    /// </summary>
     public List<SingleChoice> singleChoice { get; set; }
+
+    /// <summary>
+    /// 多选题
+    /// </summary>
     public List<MultipleChoice> multipleChoice { get; set; }
+
+    /// <summary>
+    /// 判断题
+    /// </summary>
     public List<TrueFalse> trueFalse { get; set; }
 }
 
-public  class SingleChoice
+public class SingleChoice
 {
-    public string title { get; set; } 
-    
+    public string title { get; set; }
+
     public List<string> options { get; set; }
 }
 
 public class MultipleChoice
 {
-    public string title { get; set; } 
+    public string title { get; set; }
     public List<string> options { get; set; }
 }
 
 public class TrueFalse
 {
-    public string title { get; set; } 
+    public string title { get; set; }
     public List<string> options { get; set; }
 }
