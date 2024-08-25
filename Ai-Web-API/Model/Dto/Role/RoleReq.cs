57cc9617628667713bd144a1d3e18c2adaed480f
Libihao <1074775789@qq.com>
2024-08-25 16:30:44 +0800

    feat：后端获取用户角色接口

---
+++ Ai-Web-API/Model/Dto/Role/RoleReq.cs
@@ -0,0 +1,12 @@
+﻿namespace Model.Dto.Role;
+
+/// <summary>
+/// 角色管理获取用户请求
+/// </summary>
+public class RoleReq
+{
+    /// <summary>
+    /// 用户名
+    /// </summary>
+    public string? username { get; set; }
+}
---
+++ Ai-Web-API/WebApi/Controllers/RoleManagementController.cs
@@ -0,0 +1,25 @@
+﻿using Interface;
+using Microsoft.AspNetCore.Mvc;
+using Model.Dto.Role;
+using Model.Other;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class RoleManagementController : ControllerBase
+{
+    private readonly IRoleManagementService _managementService;
+
+    public RoleManagementController(IRoleManagementService managementService)
+    {
+        _managementService = managementService;
+    }
+
+
+    [HttpGet]
+    public async Task<ApiResult> GetUserRole(RoleReq req)
+    {
+        return await _managementService.GetUserRole(req);
+    }
+}
