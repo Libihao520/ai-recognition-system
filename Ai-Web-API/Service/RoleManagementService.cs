2efa2cd2c4ea14a971f3dee3011bc1feb2acadea
Amber <1203727066@qq.com>
2024-08-26 14:19:53 +0800

    feat:软删除，更新密码

--- Ai-Web-API/Service/RoleManagementService.cs
+++ Ai-Web-API/Service/RoleManagementService.cs
@@ -13,7 +13,7 @@ public class RoleManagementService : IRoleManagementService
 {
     private MyDbContext _context;
     private IMapper _mapper;
-    
+
     public RoleManagementService(MyDbContext context, IMapper mapper)
     {
         _context = context;
@@ -36,6 +36,47 @@ public class RoleManagementService : IRoleManagementService
             var resList = _mapper.Map<List<RoleRes>>(usersEnumerable);
             return ResultHelper.Success("查询成功", resList);
         }
+    }
 
+    public async Task<ApiResult> DeleteAsync(long id)
+    {
+        try
+        {
+            // 根据id查找对象
+            var findAsync = await _context.Users.FindAsync(id);
+            // 不为空则执行软删除并且保存到数据库中
+            if (findAsync != null)
+            {
+                findAsync.IsDeleted = 1;
+                await _context.SaveChangesAsync();
+            }
+
+            return ResultHelper.Success("请求成功！", "数据已删除");
+        }
+        catch (Exception e)
+        {
+            return ResultHelper.Error("用户数据删除失败");
+        }
+    }
+
+    public async Task<ApiResult> PutPasswAsync(long id)
+    {
+        try
+        {
+            var findAsync = _context.Users.FindAsync(id);
+            if (findAsync == null)
+            {
+                return ResultHelper.Error("用户不存在 ");
+            }
+          
+            await _context.SaveChangesAsync();
+            return ResultHelper.Success("请求成功！", "密码更新完成");
+        }
+        catch (Exception e)
+        {
+            return ResultHelper.Error("更新密码时发生错误");
+        }
     }
 }
+
+    
