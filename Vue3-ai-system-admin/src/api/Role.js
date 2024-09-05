5428a8d77f3815a2018d0ce0a31fe97c5be17f00
Libihao <1074775789@qq.com>
2024-09-05 21:56:04 +0800

    feat：前端用户管理模块增删改操作

--- Vue3-ai-system-admin/src/api/Role.js
+++ Vue3-ai-system-admin/src/api/Role.js
@@ -2,3 +2,9 @@ import request from '@/utils/request'
 //获取用户角色列表
 export const getUserRoleService = (data) =>
   request.post('/RoleManagement/GetUserRole', data)
+//添加用户角色列表
+export const putUserRoleService = (data) =>
+  request.put('/RoleManagement/PutUserRole', data)
+
+  export const DeletedService = (id) =>
+  request.delete('/RoleManagement/Deleted', { params: { id } })
---
+++ Vue3-ai-system-admin/src/config/RoleManagementConfig.js
@@ -0,0 +1,5 @@
+export const genderOptions = [
+    { value: 'Administrator', label: '管理员' },
+    { value: 'Editor', label: '编辑用户' },
+    { value: 'Ordinary', label: '普通用户' }
+];  
