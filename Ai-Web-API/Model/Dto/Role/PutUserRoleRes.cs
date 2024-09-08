075a263e93aadc74038dbc2b9891bb7ee66ef6e3
Libihao <1074775789@qq.com>
2024-09-08 21:09:29 +0800

    feat：用户管理模块接口参数字符串到枚举的转换

--- Ai-Web-API/Model/Dto/Role/PutUserRoleRes.cs
+++ Ai-Web-API/Model/Dto/Role/PutUserRoleRes.cs
@@ -1,3 +1,6 @@
+using System.Text.Json.Serialization;
+using Model.Enum;
+
 namespace Model.Dto.Role;
 
 public class PutUserRoleRes
@@ -5,8 +8,11 @@ public class PutUserRoleRes
     public long? Id { get; set; }
     public string Name { get; set; }
 
+    [JsonConverter(typeof(EnumJsonConverter<AuthorizeRoleName>))]
+    public AuthorizeRoleName role { get; set; }
+
     public string Password { get; set; }
-    
+
     /// <summary>
     /// 邮箱
     /// </summary>
