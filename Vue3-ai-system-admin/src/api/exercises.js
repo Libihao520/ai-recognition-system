10a989ec8f400b40c6baa528397f9f3d669eb6ad
Libihao <1074775789@qq.com>
2024-10-07 21:09:34 +0800

    feat：练题提交接口

--- Vue3-ai-system-admin/src/api/exercises.js
+++ Vue3-ai-system-admin/src/api/exercises.js
@@ -3,3 +3,6 @@ import request from '@/utils/request'
 //获取数学题目
 export const getmMthematics = () => request.get('/Exercises/GetmMthematics')
 
+//提交
+export const postSubmitExercises = (data) =>
+  request.post('/Exercises/Submit', data)
