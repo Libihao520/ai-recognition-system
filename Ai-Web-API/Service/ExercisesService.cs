8cc0f9953ce533f85c620c7751208c2a4ecd9802
Libihao <1074775789@qq.com>
2024-10-08 21:41:09 +0800

    feat：后端单选题评改

--- Ai-Web-API/Service/ExercisesService.cs
+++ Ai-Web-API/Service/ExercisesService.cs
@@ -13,6 +13,7 @@ public class ExercisesService : IExercisesService
 {
     private MyDbContext _context;
     private readonly IMapper _mapper;
+
     public ExercisesService(MyDbContext context, IMapper mapper)
     {
         _context = context;
@@ -35,25 +36,43 @@ public class ExercisesService : IExercisesService
             {
                 var map = _mapper.Map<SingleChoice>(testPapers);
                 mathematicsRes.singleChoice.Add(map);
-            }else if (testPapers.type == (int)ExercisesType.多选题)
+            }
+            else if (testPapers.type == (int)ExercisesType.多选题)
             {
                 var map = _mapper.Map<MultipleChoice>(testPapers);
                 mathematicsRes.multipleChoice.Add(map);
-            }else if (testPapers.type == (int)ExercisesType.判断题)
+            }
+            else if (testPapers.type == (int)ExercisesType.判断题)
             {
                 var map = _mapper.Map<TrueFalse>(testPapers);
                 mathematicsRes.trueFalse.Add(map);
             }
-            
         }
-            
+
         return ResultHelper.Success("获取成功！", mathematicsRes);
     }
 
-    public Task<ApiResult> checkSubmit(SubmitExercisesReq req)
+    public async Task<ApiResult> checkSubmit(SubmitExercisesReq req)
     {
-        //TODO req 即是传过来的题目，进行批改并返回答对数量
-        throw new NotImplementedException();
-    }
+        //做对的题数
+        int count = 0;
+        var testPapersList = _context.testpapers.Where(x => x.subject == "数学").ToList();
+        var singleChoices = testPapersList.Where(q => q.type == (int)ExercisesType.单选题)
+            .Select(p => new { p.TopicNumber, answer = p.answer[0] })
+            .OrderBy(s => s.TopicNumber)
+            .ToList();
 
+        int index = 0;
+        foreach (var singleChoice in singleChoices)
+        {
+            if (singleChoice.answer == req.singleChoice[index])
+            {
+                count++;
+            }
+
+            index++;
+        }
+
+        return ResultHelper.Success("成功！", "做对题目为：" + count);
+    }
 }
