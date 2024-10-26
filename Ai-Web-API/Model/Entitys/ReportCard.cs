3f5e618523bd192dd5f26362dc1468c6f670dcfb
Libihao <1074775789@qq.com>
2024-10-26 21:04:13 +0800

    feat：添加成绩中心entity

---
+++ Ai-Web-API/Model/Entitys/ReportCard.cs
@@ -0,0 +1,34 @@
+using Model.Common;
+
+namespace Model.Entitys;
+
+/// <summary>
+/// 成绩
+/// </summary>
+public class ReportCard : IEntity
+{
+    /// <summary>
+    /// 科目
+    /// </summary>
+    public string subject { get; set; }
+
+    /// <summary>
+    /// 总分
+    /// </summary>
+    public int totalPoints { get; set; }
+
+    /// <summary>
+    /// 总题目数量
+    /// </summary>
+    public int NumberOfQuestions { get; set; }
+
+    /// <summary>
+    /// 答对数量
+    /// </summary>
+    public int CorrectQuantity { get; set; }
+
+    /// <summary>
+    /// 提交的答案
+    /// </summary>
+    public List<List<int>> SubmittedOptions { get; set; }
+}
