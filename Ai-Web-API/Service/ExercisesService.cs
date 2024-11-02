6c9865f7bdcc6b7f82e3adbc59d1ae2834d3aee2
Amber <1203727066@qq.com>
2024-11-02 19:37:49 +0800

    feat：记录用户的答题结果，用于成绩统计和分析。

--- Ai-Web-API/Service/ExercisesService.cs
+++ Ai-Web-API/Service/ExercisesService.cs
@@ -56,56 +56,69 @@ public class ExercisesService : IExercisesService
     {
         //得分
         int score = 0;
+    
         var testPapersList = _context.testpapers.Where(x => x.subject == "数学").ToList();
         var singleChoices = testPapersList.Where(q => q.type == (int)ExercisesType.单选题)
             .Select(p => new { p.TopicNumber, answer = p.answer[0],p.Grade })
             .OrderBy(s => s.TopicNumber)
             .ToList();
 
-
+        var singleChoiceCount = singleChoices.Count();
         for (int i = 0; i < Math.Min(singleChoices.Count, req.singleChoice.Count); i++)
         {
             if (singleChoices[i].answer == req.singleChoice[i])
             {
                 score=score+singleChoices[i].Grade;
+                singleChoiceCount++;
             }
         }
-
+        
         // 处理多选题
         var multipleChoices = testPapersList.Where(m => m.type == (int)ExercisesType.多选题)
             .Select(m => new { m.TopicNumber, CorrectAnswer = m.answer,m.Grade })
             .OrderBy(s => s.TopicNumber)
             .ToList();
+        int multipleChoiceCount = 0;
         for (int i = 0; i < Math.Min(multipleChoices.Count, req.multipleChoice.Count); i++)
         {
             if (multipleChoices[i].CorrectAnswer.SequenceEqual(req.multipleChoice[i]))
             {
                 score=score+multipleChoices[i].Grade;
+                multipleChoiceCount++;
             }
         }
 
         //处理判断题
         var testPapersEnumerable = testPapersList.Where(p => p.type == (int)ExercisesType.判断题)
-                .Select(p => new { p.TopicNumber, Answer = p.answer[0]==1?"true":"false",p.Grade })
-                .OrderBy(p => p.TopicNumber)
-                .ToList()
-            ;
+            .Select(p => new { p.TopicNumber, Answer = p.answer[0] == 1 ? "true" : "false", p.Grade })
+            .OrderBy(p => p.TopicNumber)
+            .ToList();
+        int trueFalseCount = 0;
         for (int i = 0; i < Math.Min(testPapersEnumerable.Count,req.trueFalse.Count); i++)
         {
             if (testPapersEnumerable[i].Answer==req.trueFalse[i])
             {
                 score=score+testPapersEnumerable[i].Grade;
+                trueFalseCount++;
             }
         }
 
-        return ResultHelper.Success("成功！", @$"本次答题得分为：{score} ,具体情况前往成绩中心查看！");
-    }
+        var singleOptions = string.Join(",",req.singleChoice);
+        var multipleOpions = string.Join(",",req.multipleChoice.SelectMany(a=>a));
+        var trueFlaseOptions = string.Join(",",req.trueFalse);
 
-    public async Task<ApiResult> SaveReportCard(ReportCard reportCard)
-    {
-        _context.ReportCards.Add(reportCard);
-
-        await _context.SaveChangesAsync();
-        return ResultHelper.Success(message: "保存成功", null);
+        ReportCard reportCard = new ReportCard
+        {
+            subject = "数学",
+            //总分
+            totalPoints = score,
+           //总数
+            NumberOfQuestions = singleChoices.Count + multipleChoices.Count + testPapersEnumerable.Count,
+            // 答对数
+            CorrectQuantity =multipleChoiceCount+trueFalseCount+singleChoiceCount,
+            // 提交的答案
+            SubmittedOptions = $"{singleOptions};{multipleOpions};{trueFlaseOptions}"
+        };      
+            return ResultHelper.Success("成功！", @$"本次答题得分为：{score} ,具体情况前往成绩中心查看！");
     }
 }
