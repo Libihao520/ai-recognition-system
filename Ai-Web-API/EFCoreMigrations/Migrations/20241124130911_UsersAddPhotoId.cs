3c5240e2e5d0b727a25c18ab31930a81b813c60c
Libihao <1074775789@qq.com>
2024-11-24 21:11:12 +0800

    feat：数据迁移

---
+++ Ai-Web-API/EFCoreMigrations/Migrations/20241124130911_UsersAddPhotoId.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersAddPhotoId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PhotosId",
+                table: "Users",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "subject",
+                table: "ReportCards",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubmittedOptions",
+                table: "ReportCards",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotosId",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "ReportCards",
+                keyColumn: "subject",
+                keyValue: null,
+                column: "subject",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "subject",
+                table: "ReportCards",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "ReportCards",
+                keyColumn: "SubmittedOptions",
+                keyValue: null,
+                column: "SubmittedOptions",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubmittedOptions",
+                table: "ReportCards",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
