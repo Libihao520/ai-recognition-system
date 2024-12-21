834442e34cc7c47a8741307fc1622d1f76ca9304
Libihao <1074775789@qq.com>
2024-12-21 16:58:19 +0800

    feat：Aigc获取数据服务

--- Ai-Web-API/Service/AigcSerevic.cs
+++ Ai-Web-API/Service/AigcSerevic.cs
@@ -1,4 +1,9 @@
+using AutoMapper;
+using EFCoreMigrations;
 using Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model;
 using Model.Dto.AiModel;
 using Model.Other;
 
@@ -6,10 +11,23 @@ namespace Service;
 
 public class AigcSerevic : IAigcSerevice
 {
-    public Task<ApiResult> GetModelService(GetModelReq req)
+    private MyDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AigcSerevic(MyDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<ApiResult> GetModelService(GetModelReq req)
     {
         // TODO 查询实体类用automap映射到GetModelRes，返回给前端（req如果有传类型和模型名称，就过滤，类型精准过滤，模型是模糊查询）
-        throw new NotImplementedException();
+        //需要分页，参考YoloService 的 getpkqTb
+        var aiModelAsync = _context.AiModels.Where(p => p.IsDeleted == 0).ToListAsync();
+        var yoloPkqResList = _mapper.Map<List<GetModelRes>>(aiModelAsync);
+
+        return ResultHelper.Success("获取成功！", yoloPkqResList);
     }
 
     public Task<ApiResult> PutModelService(PutModelReq req)
