7176c153b4722905cfbe92aabab702c2ed7b8bb9
Libihao <1074775789@qq.com>
2024-12-21 14:42:43 +0800

    feat:模型管理接口

---
+++ Ai-Web-API/Model/Dto/AiModel/GetModelReq.cs
@@ -0,0 +1,14 @@
+namespace Model.Dto.AiModel;
+
+public class GetModelReq
+{
+    /// <summary>
+    /// 模型类型
+    /// </summary>
+    public string? ModleCls { get; set; }
+
+    /// <summary>
+    /// 模型名称
+    /// </summary>
+    public string? ModelName { get; set; }
+}
