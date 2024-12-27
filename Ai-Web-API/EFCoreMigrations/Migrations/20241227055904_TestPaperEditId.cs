372a1aa2d70983bf5ece3c8151b96bf4a0fa64bc
Libihao <1074775789@qq.com>
2024-12-27 14:00:18 +0800

    feat:TestPapers主键调整

---
+++ Ai-Web-API/EFCoreMigrations/Migrations/20241227055904_TestPaperEditId.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestPaperEditId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "testpapers",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "testpapers",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "testpapers",
+                newName: "id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "testpapers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
