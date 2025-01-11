af136e2e3fbf41115720f4b19bfbaefaf37456d5
Libihao <1074775789@qq.com>
2025-01-11 22:20:53 +0800

    feat：答卷模块，成绩添加卷id，导出与提交时根据卷id处理

--- Ai-Web-API/Model/Dto/TestPapers/SubMitExercisesReq.cs
+++ Ai-Web-API/Model/Dto/TestPapers/SubMitExercisesReq.cs
@@ -2,6 +2,10 @@ namespace Model.Dto.TestPapers;
 
 public class SubMitExercisesReq
 {
+    /// <summary>
+    /// 试卷ID
+    /// </summary>
+    public long? TestPapersManageId { get; set; }
     public List<int>? SingleChoice { get; set; }
     public List<List<int>>? MultipleChoice { get; set; }
     public List<string>? TrueFalse { get; set; }
