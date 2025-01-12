114c5ffdfdb91348fbd05d8c996d4f4a5d382a22
Libihao <1074775789@qq.com>
2025-01-12 15:20:32 +0800

    feat：数据库迁移

---
+++ Ai-Web-API/EFCoreMigrations/Migrations/20250112071948_YoloTbsAddSbjg.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class YoloTbsAddSbjg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SbJg",
+                table: "YoloTbs",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SbJg",
+                table: "YoloTbs");
+        }
+    }
+}
