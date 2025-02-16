63208609cd783971649a5e436aeff42628f3dd74
Libihao <1074775789@qq.com>
2025-02-16 18:06:18 +0800

    feat：初始化数据

--- Ai-Web-API/WebApi/Controllers/ToolController.cs
+++ Ai-Web-API/WebApi/Controllers/ToolController.cs
@@ -1,6 +1,8 @@
+using CommonUtil;
 using EFCoreMigrations;
 using Microsoft.AspNetCore.Mvc;
 using Model.Entities;
+using Model.Enum;
 
 namespace WebApi.Controllers;
 
@@ -15,47 +17,69 @@ public class ToolController : ControllerBase
         _context = context;
     }
 
+    /// <summary>
+    /// 初始化值
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     public string InitDateBase()
     {
-        #region 创建初始化值
-
+        //用户
         var users = new List<Users>()
         {
             new Users
             {
-                Name = "lbh",
-                PassWord = "123456",
+                Id = 1823648887341056,
+                Name = "lbhlbh",
+                PassWord = AesUtilities.Encrypt("123456"),
+                Email = AesUtilities.Encrypt("1074775781@qq.com"),
+                Role = AuthorizeRoleName.Administrator,
+                CreateUserId = 001,
                 CreateDate = DateTime.Now,
-                CreateUserId = 0,
                 IsDeleted = 0
             },
             new Users
             {
-                Name = "yhj",
-                PassWord = "1234567",
+                Id = 1823648984858624,
+                Name = "yhjyhj",
+                PassWord = AesUtilities.Encrypt("123456"),
+                Email = AesUtilities.Encrypt("1074775782@qq.com"),
+                Role = AuthorizeRoleName.Administrator,
+                CreateUserId = 001,
                 CreateDate = DateTime.Now,
-                CreateUserId = 1,
+                IsDeleted = 0
+            }
+        };
+        var aiModels = new List<AiModels>()
+        {
+            new AiModels()
+            {
+                Id = 1813709254033409,
+                Path = @$"Model\pkq.onnx",
+                ModelCls = "目标监测",
+                ModelName = "皮卡丘识别",
+                ModelSize = 43,
+                CreateUserId = 1823648887341056,
+                CreateDate = DateTime.Now,
+                IsDeleted = 0
+            },
+            new AiModels()
+            {
+                Id = 1813709254033410,
+                Path = @$"Model\animal.onnx",
+                ModelCls = "图像分类",
+                ModelName = "动物识别",
+                ModelSize = 214,
+                CreateUserId = 1823648887341056,
+                CreateDate = DateTime.Now,
                 IsDeleted = 0
             }
         };
 
-        #endregion
-
-
-        #region 添加到数据库
-
         _context.Users.AddRange(users);
-        //_context.Users.AddRange(new[] { user1, user2 });
-        // _context.yolotbs.Add(yolotbs);
-
-        #endregion
-
-        #region 保存
-
+        _context.AiModels.AddRange(aiModels);
         _context.SaveChanges();
 
-        #endregion
 
         return "ok";
     }
