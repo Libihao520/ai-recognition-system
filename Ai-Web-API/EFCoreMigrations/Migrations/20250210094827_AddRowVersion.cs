a95b47aebb0ef2023681aa85070444ec1244d882
Libihao <1074775789@qq.com>
2025-02-16 18:09:00 +0800

    feat：并发优化，数据库增加RowVersion（版本字段）

---
+++ Ai-Web-API/EFCoreMigrations/Migrations/20250210094827_AddRowVersion.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "YoLoTbs",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "Users",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "TestPapersManages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "ReportCards",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "AiModels",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "YoLoTbs");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "TestPapersManages");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "ReportCards");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "AiModels");
+        }
+    }
+}
