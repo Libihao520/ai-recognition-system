bbfeeb3931afa0f6757bda1edcb8b44e8540ae6f
Libihao <1074775789@qq.com>
2025-03-22 13:12:25 +0800

    feat:依赖注入调整

--- Ai-Web-API/WebApi/Config/AutofacModuleRegister.cs
+++ Ai-Web-API/WebApi/Config/AutofacModuleRegister.cs
@@ -1,15 +1,15 @@
 using System.Reflection;
 using Autofac;
+using CommonUtil;
 using CommonUtil.AiGcUtil;
 
 namespace WebApi.Config;
 
 public class AutofacModuleRegister : Autofac.Module
 {
-    //注册接口和实现之间的关系
     protected override void Load(ContainerBuilder builder)
     {
-        //通过反射
+        //通过反射注册接口和实现之间的关系
         Assembly interfaceAssembly = Assembly.Load("Interface");
         Assembly serviceAssembly = Assembly.Load("Service");
         builder.RegisterAssemblyTypes(interfaceAssembly, serviceAssembly).AsImplementedInterfaces();
@@ -19,5 +19,9 @@ public class AutofacModuleRegister : Autofac.Module
         builder.RegisterType<AiRequestStrategyFactory>().AsSelf().SingleInstance();
         //讯飞API实现注册
         builder.RegisterType<SparkAiRequestStrategy>().AsSelf().InstancePerDependency();
+        //发送邮箱工具
+        builder.RegisterType<EmailUtil>().AsSelf().SingleInstance();
+        //获取用户信息工具
+        builder.RegisterType<UserInformationUtil>().AsSelf().SingleInstance();
     }
 }
